0f9bc3044f3ca37164b049fad4d6391a8fd5f05e
Eyüp Şengöz <95869100+LewisLosa@users.noreply.github.com>
2026-01-11 03:55:43 +0300

    Update desktop.nix

--- modules/nixos/desktop.nix
+++ modules/nixos/desktop.nix
@@ -3,22 +3,29 @@
   vars,
   ...
 }: {
-  services = {
-    xserver = {
-      enable = true;
-      xkb = {
-        layout = "tr";
-        variant = "";
-      };
-      desktopManager.gdm.enable = {
-        enable = true;
-        wayland = true;
-      };
-      desktopManager.gnome.enable = true;
-    };
-    udev.packages = with pkgs; [gnome-settings-daemon];
+  services.xserver = {
+    enable = true;
 
-    libinput.enable = true;
-    programs.ssh.enableAskPassword = true;
-    environment.variables.SSH_ASKPASS_REQUIRE = "prefer";
+    xkb = {
+      layout = "tr";
+      variant = "";
+    };
+
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
+    };
+
+    desktopManager.gnome.enable = true;
+  };
+
+  services.udev.packages = with pkgs; [
+    gnome-settings-daemon
+  ];
+
+  services.libinput.enable = true;
+
+  programs.ssh.enableAskPassword = true;
+
+  environment.variables.SSH_ASKPASS_REQUIRE = "prefer";
 }
