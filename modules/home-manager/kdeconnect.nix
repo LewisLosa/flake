efa2bddf0898c933bb536a42dd13afa1ee516471
Eyüp Şengöz <lewislosa@gmail.com>
2026-01-18 19:03:23 +0300

    feat: KdeConnect added.

---
+++ modules/home-manager/kdeconnect.nix
@@ -0,0 +1,14 @@
+{
+  services = {
+    kdeConnect.enable = true;
+  };
+  networking.firewall = rec {
+    allowedTCPPortRanges = [
+      {
+        from = 1714;
+        to = 1764;
+      }
+    ];
+    allowedUDPPortRanges = allowedTCPPortRanges;
+  };
+}
