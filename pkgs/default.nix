6fa118e15797ed782d59eb01810baea0f122e686
Eyüp Şengöz <lewislosa@gmail.com>
2026-01-18 15:49:26 +0300

    feat: aya added and cider removed.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,10 @@
 # inspo. https://codeberg.org/Ti-mmm/nixos-config/src/branch/main/pkgs/cider3/default.nix
-{pkgs, ...}: let
-  cider3 = import ./cider {inherit pkgs;};
-in {
+{ pkgs, ... }:
+let
+  aya = import ./aya { inherit pkgs; };
+in
+{
   environment.systemPackages = [
-    cider3
+    aya
   ];
 }
