151ca975b3ca463bae3064f856e993ec29b238e8
Eyüp Şengöz <lewislosa@gmail.com>
2026-01-19 21:56:13 +0300

    fix.

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -3,6 +3,7 @@
     ./fonts.nix
     ./apps
   ];
+  # inspo: https://github.com/microsoft/vscode/issues/187338#issuecomment-1626487441
   home.file.".vscode/argv.json".text = ''
     {
       // Fixes the "an OS keyring couldn't be identified for
