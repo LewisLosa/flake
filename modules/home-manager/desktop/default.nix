e052b02f1e7bfc6d0abdb0478b95b1f9b1960093
Eyüp Şengöz <lewislosa@gmail.com>
2026-01-19 21:56:09 +0300

    fix: vscode encryption error.

--- modules/home-manager/desktop/default.nix
+++ modules/home-manager/desktop/default.nix
@@ -3,4 +3,11 @@
     ./fonts.nix
     ./apps
   ];
+  home.file.".vscode/argv.json".text = ''
+    {
+      // Fixes the "an OS keyring couldn't be identified for
+      // storing the encryption..." error
+      "password-store": "gnome-libsecret"
+    }
+  '';
 }
