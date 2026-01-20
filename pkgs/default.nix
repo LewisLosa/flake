a0fe8d6134893203455a98ee2dd199ac74b01614
Eyüp Şengöz <lewislosa@gmail.com>
2026-01-20 13:30:17 +0300

    feat: windscribe package added

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,11 @@
-{pkgs, ...}: let
-  aya = import ./aya {inherit pkgs;};
-in {
-  environment.systemPackages = [aya];
+{ pkgs, ... }:
+let
+  aya = import ./aya { inherit pkgs; };
+  windscribe = import ./windscribe { inherit pkgs; };
+in
+{
+  environment.systemPackages = [
+    aya
+    windscribe
+  ];
 }
