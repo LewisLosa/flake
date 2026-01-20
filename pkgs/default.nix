b1def4a82099b5f63a7b2469e3f5de5455e6c672
Eyüp Şengöz <lewislosa@gmail.com>
2026-01-20 13:53:58 +0300

    fix: not found error.

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,11 +1,9 @@
 { pkgs, ... }:
 let
   aya = import ./aya { inherit pkgs; };
-  windscribe = pkgs.callPackage ./windscribe { };
 in
 {
   environment.systemPackages = [
     aya
-    windscribe
   ];
 }
